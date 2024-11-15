8117af1ae3cd2b5b08edec17bafe33b199af8438
vizhozhh <23020580@vnu.edu.vn>
2024-11-15 09:54:38 +0700

    Update Controller.java

--- src/main/java/com/library/Controller.java
+++ src/main/java/com/library/Controller.java
@@ -1,4 +1,5 @@
 package com.library;
+
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -7,8 +8,6 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
-import javax.smartcardio.Card;
-import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -19,7 +18,6 @@ public class Controller implements Initializable {
     @FXML
     private HBox cardLayout;
 
-
     @FXML
     private GridPane bookContainer;
 
@@ -28,141 +26,48 @@ public class Controller implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        recentlyAdded = new ArrayList<>(recentlyAdded());
-        recommended = new ArrayList<>(books());
+        // Kết nối đến cơ sở dữ liệu
+        DatabaseConnection.connectUserAccount(); // Nếu cần thiết
+
+        List<Book> allBooks = DatabaseConnection.getBooks();
+        // Lấy tất cả sách từ cơ sở dữ liệu
+        recentlyAdded = new ArrayList<>(allBooks); // Giả sử bạn muốn hiển thị tất cả sách ở đây
+        recommended = new ArrayList<>(allBooks); // Tương tự cho sách được đề xuất
+
         int column = 0;
         int row = 1;
 
+
         try {
-
-        for (Book value : recentlyAdded) {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("card-view.fxml"));
-            HBox cardBox = fxmlLoader.load();
-            CardController cardController = fxmlLoader.getController();
-            cardController.setData(value);
-            cardLayout.getChildren().add(cardBox);
-        }
-
-        for (Book book : recommended) {
-            FXMLLoader fxmlLoader = new FXMLLoader();
-            fxmlLoader.setLocation(getClass().getResource("Book-view.fxml"));
-            VBox bookBox = fxmlLoader.load();
-            BookController bookController = fxmlLoader.getController();
-            bookController.setData(book);
-
-            if(column == 6) {
-                column = 0;
-                row++;
+            // Hiển thị sách mới thêm
+            for (Book value : recentlyAdded) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("card-view.fxml"));
+                HBox cardBox = fxmlLoader.load();
+                CardController cardController = fxmlLoader.getController();
+                cardController.setData(value);
+                cardLayout.getChildren().add(cardBox);
             }
 
-            bookContainer.add(bookBox, column++, row);
-            GridPane.setMargin(bookBox, new Insets(10));
+            // Hiển thị sách được đề xuất
+            for (Book book : recommended) {
+                FXMLLoader fxmlLoader = new FXMLLoader();
+                fxmlLoader.setLocation(getClass().getResource("Book-view.fxml"));
+                VBox bookBox = fxmlLoader.load();
+                BookController bookController = fxmlLoader.getController();
+                bookController.setData(book);
 
+                if (column == 6) {
+                    column = 0;
+                    row++;
+                }
 
-        }
+                bookContainer.add(bookBox, column++, row);
+                GridPane.setMargin(bookBox, new Insets(10));
+            }
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
     }
-
-    private List<Book> recentlyAdded() {
-        List<Book> ls = new ArrayList<>();
-        Book book = new Book();
-        book.setTitle("Đắc Nhân Tâm");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/dacnhanhtam.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        return ls;
-
-
-    };
-
-    private List<Book> books() {
-        List<Book> ls = new ArrayList<>();
-        Book book = new Book();
-        book.setTitle("Đắc Nhân Tâm");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/dacnhanhtam.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("CHA GIÀU CHA NGHÈO");
-        book.setAuthor("Tác giả");
-        book.setImageSrc("/ScreenUI/Picture/chagiauchangheo.jpg");
-        ls.add(book);
-
-        book = new Book();
-        book.setTitle("NNKK");
-        book.setAuthor("Victorrigo");
-        book.setImageSrc("/ScreenUI/Picture/nhungnguoikhonkho.jpg");
-        ls.add(book);
-
-        return ls;
-    }
-
-
 }
