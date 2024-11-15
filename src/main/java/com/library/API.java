3dc6b4f4713382ccd37d14ad1b8940657d8cf9a3
Sntug <https://github.com/Sntug>
2024-11-15 20:23:52 +0700

    copy cua Tung

---
+++ src/main/java/com/library/API.java
@@ -0,0 +1,9 @@
+package com.library;
+
+public class API {
+    private static final String API_KEY = "AIzaSyCWGMY_SLPzhwUugEoyeZF2WOa5KodzvDI";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
