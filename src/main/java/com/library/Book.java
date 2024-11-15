13ed72dd117f6f9f16a7d4c8783974b7a50743c2
vizhozhh <23020580@vnu.edu.vn>
2024-11-15 21:53:00 +0700

    Update Book.java

--- src/main/java/com/library/Book.java
+++ src/main/java/com/library/Book.java
@@ -1,58 +1,83 @@
 package com.library;
 
+import javafx.scene.control.CheckBox;
+
+
 public class Book {
+    private CheckBox checkBox;
     private String ISBN;
     private String title;
     private String author;
-    private String ImageSrc;
-    private String year;
+    private byte[] bookImage;
+    private int year;
+    private String description;
+    private int available;
 
-    public Book() {
 
-    }
-
-    public String getImageSrc() {
-        return ImageSrc;
-    }
-
-    public void setImageSrc(String imageSrc) {
-        ImageSrc = imageSrc;
-    }
-
-    public Book(String ISBN, String title, String author, String year) {
+    public Book(String ISBN, String title, String author, int year) {
         this.ISBN = ISBN;
         this.title = title;
         this.author = author;
         this.year = year;
     }
 
-    public String getTitle() {
+    public Book() {
 
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN(String ISBN) {
+        this.ISBN = ISBN;
+    }
+
+    public String getTitle() {
         return title;
     }
 
     public void setTitle(String title) {
-
         this.title = title;
     }
 
     public String getAuthor() {
-
         return author;
     }
 
     public void setAuthor(String author) {
-
         this.author = author;
     }
 
-    public String getYear() {
+    public byte[] getBookImage() {
+        return bookImage;
+    }
 
+    public void setImageSrc(byte[] bookImage) {
+        this.bookImage = bookImage; // Gán mảng byte cho bookImage
+    }
+
+    public int getYear() {
         return year;
     }
 
-    public void setYear(String year) {
-
+    public void setYear(int year) {
         this.year = year;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAvailable() {
+        return available;
+    }
+
+    public void setAvailable(int available) {
+        this.available = available;
     }
 }
