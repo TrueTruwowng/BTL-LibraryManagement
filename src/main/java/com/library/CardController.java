7928311e3d7c697f696cd63ae81d10307b0757a4
vizhozhh <23020580@vnu.edu.vn>
2024-11-15 09:57:03 +0700

    Update CardController.java

--- src/main/java/com/library/CardController.java
+++ src/main/java/com/library/CardController.java
@@ -25,10 +25,15 @@ public class CardController {
     private String[] colors = {"B9E5FF", "BDB2FE", "FB9AA8", "FF5056"};
 
     public void setData(Book book) {
-        Image image = new Image(Objects.requireNonNull(getClass().getResourceAsStream(book.getImageSrc())));
-        bookImage.setImage(image);
         bookTitle.setText(book.getTitle());
         authorName.setText(book.getAuthor());
+
+        if (book.getImageSrc() != null) {
+            bookImage.setImage(new Image(book.getImageSrc()));
+        } else {
+
+            bookImage.setImage(new Image(getClass().getResourceAsStream("/ScreenUI/Picture/NULLimage.jpg")));
+        }
 
         String color = colors[(int) (Math.random() * colors.length)];
         box.setStyle("-fx-background-color: #" + color + ";" +
