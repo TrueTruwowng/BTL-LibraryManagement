d1501950917faefef811a37bd98ef45aa1503dd5
vizhozhh <23020580@vnu.edu.vn>
2024-11-16 23:21:02 +0700

    Update DashBoardController.java

--- src/main/java/com/library/DashBoardController.java
+++ src/main/java/com/library/DashBoardController.java
@@ -8,10 +8,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.net.URL;
